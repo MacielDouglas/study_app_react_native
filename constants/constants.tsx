980963d6a5c78584c7483a302b6e4d1d1b3e27bc
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-06-13 09:11:50 -0300

    Criado app, add telas iniciais

---
+++ constants/constants.tsx
@@ -0,0 +1,24 @@
+export const onboardingSwiperData: onboardingSwiperDataType[] = [
+  {
+    id: 1,
+    title: "Start your learning journey with dose of fun!",
+    description: "Our engaging learning videos will spark",
+    sortDescription: "your curiosity & enjoyable.",
+    image: require("@/assets/onboarding/intro_1.png"),
+  },
+  {
+    id: 2,
+    title: "Discover you passion, elevate your expertise",
+    description: "Our comprehensive learning videos will",
+    sortDescription: "instructors to guide every step.",
+    image: require("@/assets/onboarding/intro_2.png"),
+  },
+  {
+    id: 3,
+    title: "Get online certificate with Unusual skills",
+    description: "To earn an online certificate, individuals",
+    sortDescription: "typically need to complete the required.",
+    sortDescription2: "coursework & assessments",
+    image: require("@/assets/onboarding/intro_3.png"),
+  },
+];
