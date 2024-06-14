af0cb7d6b4e790f14246d6f37fb79562ade2a3cd
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-06-14 09:16:58 -0300

    Terminado tela de login,

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -51,6 +51,7 @@ function RootLayoutNav() {
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" />
           <Stack.Screen name="(routes)/welcome-intro/index" />
+          <Stack.Screen name="(routes)/login/index" />
         </Stack>
       )}
     </>
